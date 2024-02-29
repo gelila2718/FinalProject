d4ff41f37f8731aacb2dcb3d8a5087f7935d8dc9
Gelila Demis <gelilademis@gmail.com>
2024-02-29 21:55:18 +0300

    last update

--- myweb/urls.py
+++ myweb/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from . import views
-
+from django.conf import settings
+from django.conf.urls.static import static
 # from . import views
 
 urlpatterns = [
-    path("", views.home, name="home"),
-    path("about/", views.AboutPageView, name="about"),
+    path('', views.home, name="home"),
+    path('about/', views.AboutPageView, name="about"),
+    path("contact/", views.ContactPageView, name="contact"),
     path("store/", views.StorePageView, name="store"),
-]
+    path('view_item/<int:product_id>/', views.view_item, name='view_item'),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- myweb/views.py
+++ myweb/views.py
@@ -1,15 +1,32 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, get_object_or_404
 from django.views.generic import View
 from .models import Product
 
 
-# Create your views here.
+# def home(request):
+#     Products = Product.objects.all()[:1]
+#     context = {"featured_products": Products}
+#     return render(request, "home.html", context)
+
+
 def home(request):
-    return render(request, "home.html")
+    products = Product.objects.all()  # Assuming you want to display all products
+    return render(request, "home.html", {"products": products})
 
 
 def AboutPageView(request):
     return render(request, "about.html")
+
+
+def ContactPageView(request):
+    return render(request, "contact.html")
+
+
+def view_item(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    all_products = Product.objects.all()
+    return render(request, "view_item.html", {"product": product})
+
 
 def StorePageView(request):
     products = Product.objects.all()
